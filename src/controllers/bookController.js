2a3c20e4de7e04c3dbcaa73f3c42b7f94684f89d
shivansh-p7 <shivanshsharma55364@gmail.com>
2023-01-23 12:49:25 +0530

    controller changes

---
+++ src/controllers/bookController.js
@@ -0,0 +1,21 @@
+const BookModel=require("../Models/bookModel");
+const userModel=require("../Models/userModel");
+
+
+
+const createBook= async (req,res)=>{
+
+
+
+
+}
+
+
+const getBook= async (req,res)=>{
+
+
+
+
+}
+
+module.exports={createBook,getBook}
---
+++ src/controllers/userController.js
@@ -0,0 +1,21 @@
+const userModel=require("../Models/userModel");
+const BookModel=require("../Models/bookModel");
+
+
+const createUser= async (req,res)=>{
+
+
+
+
+}
+
+
+const userLogin= async (req,res)=>{
+
+
+
+
+
+}
+
+module.exports={createUser,userLogin}
